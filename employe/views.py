575441744551736a6c48fe9f4d9857d4a70c5583
Maikoke <mamadoumaikoke36@gmail.com>
2023-07-17 11:54:48 +0100

    research features

--- employe/views.py
+++ employe/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 from employe.models import Employe
 
+from django.db.models import Q
+
 from django.contrib import messages
 
 from django.core.paginator import Paginator
@@ -30,8 +32,13 @@ def employes(request):
         
 
     formEmp = forms.EmployeForm()
-    # employes = Employe.objects.all()   
-    employes = Employe.objects.order_by('idEmploye').all()
+    # employes = Employe.objects.all()  
+
+    # Handle search query
+    search_query = request.GET.get('search_query')
+    employes = get_filtered_employes(search_query)
+
+    # = Employe.objects.order_by('idEmploye').all()
 
     page = Paginator(employes, 1)
     page_liste = request.GET.get('page')
@@ -66,3 +73,17 @@ def edit_employee(request, employee_id):
             messages.success(request, "Les infos de l'employé " + employee.nom +" sont modifiés")
     
     return redirect('employes')
+
+
+
+def get_filtered_employes(search_query):
+    if search_query:
+        return Employe.objects.filter(
+            Q(nom__icontains=search_query) |
+            Q(prenom__icontains=search_query) |
+            Q(dateEmbauche__icontains=search_query) |
+            Q(poste__icontains=search_query) |
+            Q(email__icontains=search_query)
+        ).order_by('idEmploye')
+    else:
+        return Employe.objects.all().order_by('idEmploye')
--- fournisseur/views.py
+++ fournisseur/views.py
@@ -4,19 +4,25 @@ from .forms import FournisseurForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 @login_required
 def fournisseurs_page(request):
+    query = request.GET.get('search_query')
+    if query:
+        fournisseurs_list = Fournisseur.objects.filter(Q(nomFournisseur__icontains=query) | Q(adresse__icontains=query) | Q(email__icontains=query) | Q(telephone__icontains=query))
+    else:
+        fournisseurs_list = Fournisseur.objects.all()
+
     if request.method == "POST":
         form = FournisseurForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Le fournisseur a été ajouté avec succès.")
             return redirect('fournisseurs')
     else:
         form = FournisseurForm()
-        
-    fournisseurs_list = Fournisseur.objects.all()
     
     paginator = Paginator(fournisseurs_list, 5)
     
@@ -24,8 +30,30 @@ def fournisseurs_page(request):
     
     fournisseurs = paginator.get_page(page_number)
     
-    #fournisseurs = Fournisseur.objects.all()
-    return render(request, 'fournisseur.html', {'fournisseurs': fournisseurs, 'form': form})
+    return render(request, 'fournisseur.html', {'fournisseurs': fournisseurs, 'form': form, 'search_query': query})
+    
+
+
+# @login_required
+# def fournisseurs_page(request):
+#     if request.method == "POST":
+#         form = FournisseurForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('fournisseurs')
+#     else:
+#         form = FournisseurForm()
+        
+#     fournisseurs_list = Fournisseur.objects.all()
+    
+#     paginator = Paginator(fournisseurs_list, 5)
+    
+#     page_number = request.GET.get('page')
+    
+#     fournisseurs = paginator.get_page(page_number)
+    
+#     #fournisseurs = Fournisseur.objects.all()
+#     return render(request, 'fournisseur.html', {'fournisseurs': fournisseurs, 'form': form})
 
 
 
